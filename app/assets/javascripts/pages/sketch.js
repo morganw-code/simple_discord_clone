f6ef9434d1ef4fe47c239f6c21ac90fa2b92536d
Morgan Webb <mnbwebb2233@gmail.com>
2020-05-01 22:12:50 +1000

    Create p5 canvas container and draw test sketch

--- app/assets/javascripts/pages/sketch.js
+++ app/assets/javascripts/pages/sketch.js
@@ -1,9 +1,37 @@
+var positions = { };
+var entries = 0;
+var width = 0;
+var height = 0;
+
+function generatePositions() {
+    for(n = 0; n < 10000; n++) {
+        positions[n] = new Array(Math.floor(Math.random() * width), Math.floor(Math.random() * height));
+    }
+}
+
 function setup() {
-    var canvas = createCanvas(100, 100);
-    canvas.parent('p5-canvas-holder');
+    let container = document.getElementById("p5-canvas-holder");
+    width = container.offsetWidth;
+    height = container.offsetHeight;
+    let canvas = createCanvas(width, height);
+    canvas.parent("p5-canvas-holder");
     console.log("p5 loaded");
+    generatePositions();
+    entries = Object.entries(positions);
 }
 
 function draw() {
-    ellipse(50, 50, 80, 80);
+    clear();
+    for(const [n, arr] of entries) {
+        ellipse(arr[0], arr[1], 5, 5);
+    }
+
+    // updatePositions();
+}
+
+function updatePositions() {
+    for([n, arr] of entries) {
+        arr[0] += 1;
+        arr[1] += 1;
+    }
 }
